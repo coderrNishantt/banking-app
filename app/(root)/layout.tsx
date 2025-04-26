b024718acb8c83596c4be6d486e4724e676a3fac
coderrNishantt <nishntkumr627@gmail.com>
2025-04-26 12:53:55 +0530

    implented initial folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import '../globals.css';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <main>
+          SIDEBAR
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
